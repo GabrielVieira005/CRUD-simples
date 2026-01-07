cb12f2529524d8ae6b6000ad753bb3dea9fd83bd
GabrielVieira005 <gabrielvieiracosta80@gmail.com>
2026-01-07 00:49:00 -0300

    primeiro commit

---
+++ api/pessoasAPI.js
@@ -0,0 +1,44 @@
+import { MongoClient, ObjectId } from "mongodb";
+
+const client = new MongoClient(process.env.MONGO_URI)
+let db
+
+async function connectDB() {
+    if(!db){
+        await client.connect()
+        db = client.db("TABELA")
+    }
+    return db
+}
+
+export default async function handler(req, res) {
+    const db = await connectDB()
+    const collection = db.collection("Pessoas")
+
+    if(req.method === "GET"){
+        const pessoa = await collection.find({}).toArray()
+        return res.status(200).json(pessoa)
+    }
+
+    else if(req.method === "POST"){
+        const {name, phone, cpf, age} = req.body
+
+        if(!name) return res.status(400).json({message: "Nome é obrigatório"})
+        if(!phone) return res.status(400).json({message: "Número de telefone é obrigatório"})
+        if(!cpf) return res.status(400).json({message: "CPF é obrigatório"})
+        if(!age) return res.status(400).json({message: "Idade é obrigatório"})
+
+        const result = await collection.insertOne({
+            name: name.trim(),
+            phone: phone.trim(),
+            cpf: cpf.trim(),
+            age: age.trim()
+        })
+
+        return res.status(201).json({
+            message: "Pessoa adicionada com sucesso",
+            id: result.insertId
+        })
+    }
+    
+}
---
+++ public/pessoasSCRIPT.js
@@ -0,0 +1,105 @@
+
+const API = "../api/pessoasAPI"
+
+async function carregarPessoas() {
+    try{
+        const res = await fetch(API)
+
+        if(!res.ok){
+            throw new Error(`Erro HTTP : ${res.status}`)
+        }
+
+        const pessoas = await res.json()
+        const ul = document.getElementById("pessoas-list")
+        ul.innerHTML = ""
+
+        if(pessoas.length === 0){
+            const li = document.createElement("li")
+
+            li.className = "empty-message"
+            li.innerHTML = "Nenhuma pessoa adicionada no momento"
+            ul.appendChild(li)
+            return
+        }
+
+        pessoas.forEach(p => {
+
+            const pessoaContent = document.createElement("div")
+            pessoaContent.className = "pessoa-content"
+
+            const name = document.createElement("div")
+            name.className = "nomePessoa"
+            name.textContent = p.nome
+
+            const phone = document.createElement("div")
+            phone.className = "telefonePessoa"
+            phone.textContent = p.telefone
+
+            const cpf = document.createElement("div")
+            cpf.className = "cpfPessoa"
+            cpf.textContent = p.cpf
+
+            const idade = document.createElement("div")
+            idade.className = "idadePessoa"
+            idade.textContent = p.idade
+
+            pessoaContent.appendChild(name)
+            pessoaContent.appendChild(phone)
+            pessoaContent.appendChild(cpf)
+            pessoaContent.appendChild(idade)   
+            
+            //Faltou criar botão de deletar e editar pessoas(Feature pra depois)
+        });
+
+    }catch(error){
+        console.error("Erro ao carregar tarefas")
+        const ul = document.getElementById("pessoas-list")
+        ul.innerHTML = '<li id="empty-message">Erro ao carregar as pessoas</li>'
+        }
+    }
+
+    async function adicionarPessoa() {
+        const nameInput = document.getElementById("nomeInput")
+        const telefoneInput = document.getElementById("telefoneInput")
+        const cpfInput = document.getElementById("cpfInput")
+        const idadeInput = document.getElementById("idadeInput")
+
+        if(!nameInput.value || !telefoneInput.value || !cpfInput.value || !idadeInput.value){
+            alert("Todos os campos são obrigatorios")
+            return
+        }
+
+        //Depois fazer regex para numero de celular e CPF
+
+        try{
+            const response = await fetch(API,{
+                method: "POST",
+                headers: {"Content-Type": "aplication/json"},
+                body: JSON.stringfy({
+                    nome: nameInput.value,
+                    telefone: telefoneInput.value,
+                    cpf: cpfInput.value,
+                    idade: idadeInput.value
+                })
+            })
+
+            const data = await response.json()
+
+            if(!response.ok){
+                alert(data.message || "Erro ao adicionar pessoa")
+            }
+            
+            nameInput.innerHTML = ""
+            telefoneInput.innerHTML = ""
+            cpfInput.innerHTML = ""
+            idadeInput.innerHTML = ""
+            carregarPessoas()
+            alert("Pessoa adicionada com sucesso")
+        }catch(error){
+            console.error("Erro", error)
+            alert("Erro ao adicionar pessoa")
+        }
+    
+    }
+
+    document.getElementById("add").addEventListener("click", adicionarPessoa())
